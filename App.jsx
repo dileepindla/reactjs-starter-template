d15d03d8be37ad9fb244fa949c1643ccc73b163d
Dileepindla <indladileep1@gmail.com>
2017-05-04 13:43:08 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+class App extends Component {
+   render() {
+      return (
+         <div>
+            Hello World!! by Watchdown.com
+         </div>
+      );
+   }
+}
+export default App;
